dcade8302740bb4efbd0089e01752af2220c3032
Bjorn James Ferrer <bvferrer@pjlhuillier.com>
2024-08-09 11:52:37 +0800

    TrainingRoomScheduleMapping remove unused code/comment paramter count.

--- iReserveWS/App_Code/TrainingRoomScheduleMapping.cs
+++ iReserveWS/App_Code/TrainingRoomScheduleMapping.cs
@@ -9,155 +9,155 @@ using System.Data;
 /// </summary>
 public class TrainingRoomScheduleMapping
 {
-    public TrainingRoomScheduleMapping()
+  public TrainingRoomScheduleMapping()
+  {
+    //
+    // TODO: Add constructor logic here
+    //
+  }
+
+  #region Fields/Properties
+
+  private int _mappingID;
+
+  public int MappingID
+  {
+    get { return _mappingID; }
+    set { _mappingID = value; }
+  }
+
+  private int _partitionID;
+
+  public int PartitionID
+  {
+    get { return _partitionID; }
+    set { _partitionID = value; }
+  }
+
+  private DateTime _date;
+
+  public DateTime Date
+  {
+    get { return _date; }
+    set { _date = value; }
+  }
+
+  private string _referenceNumber;
+
+  public string ReferenceNumber
+  {
+    get { return _referenceNumber; }
+    set { _referenceNumber = value; }
+  }
+
+  private string _isCancelled;
+
+  public string IsCancelled
+  {
+    get { return _isCancelled; }
+    set { _isCancelled = value; }
+  }
+
+  #endregion
+
+  #region Methods
+
+  public void InsertTrainingRoomScheduleMapping(SqlConnection sqlConnection)
+  {
+    using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.InsertTrainingRoomScheduleMapping, sqlConnection))
     {
-        //
-        // TODO: Add constructor logic here
-        //
+      sqlCommand.CommandType = CommandType.StoredProcedure;
+      sqlCommand.Parameters.AddWithValue("@partitionID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(this.PartitionID));
+      sqlCommand.Parameters.AddWithValue("@date", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(this.Date));
+      sqlCommand.Parameters.AddWithValue("@refNo", RDFramework.Utility.Conversion.SafeSetDatabaseValue<string>(this.ReferenceNumber));
+      sqlCommand.ExecuteNonQuery();
     }
+  }
 
-    #region Fields/Properties
+  public bool ValidateTrainingRoomScheduleAvailability(int roomID, DateTime startDate, DateTime endDate)
+  {
+    bool validationStatus = false;
 
-    private int _mappingID;
-
-    public int MappingID
+    using (SqlConnection sqlConnection = new SqlConnection(Settings.iReserveConnectionStringReader))
     {
-        get { return _mappingID; }
-        set { _mappingID = value; }
-    }
+      using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.ValidateTrainingRoomScheduleAvailability, sqlConnection))
+      {
+        sqlConnection.Open();
+        sqlCommand.CommandType = CommandType.StoredProcedure;
+        sqlCommand.Parameters.AddWithValue("@roomID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(roomID));
+        sqlCommand.Parameters.AddWithValue("@startDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(startDate));
+        sqlCommand.Parameters.AddWithValue("@endDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(endDate));
 
-    private int _partitionID;
-
-    public int PartitionID
-    {
-        get { return _partitionID; }
-        set { _partitionID = value; }
-    }
-
-    private DateTime _date;
-
-    public DateTime Date
-    {
-        get { return _date; }
-        set { _date = value; }
-    }
-
-    private string _referenceNumber;
-
-    public string ReferenceNumber
-    {
-        get { return _referenceNumber; }
-        set { _referenceNumber = value; }
-    }
-
-    private string _isCancelled;
-
-    public string IsCancelled
-    {
-        get { return _isCancelled; }
-        set { _isCancelled = value; }
-    }
-
-    #endregion
-
-    #region Methods
-
-    public void InsertTrainingRoomScheduleMapping(SqlConnection sqlConnection)
-    {
-        using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.InsertTrainingRoomScheduleMapping, sqlConnection))
+        using (SqlDataReader rd = sqlCommand.ExecuteReader())
         {
-            sqlCommand.CommandType = CommandType.StoredProcedure;
-            sqlCommand.Parameters.AddWithValue("@partitionID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(this.PartitionID));
-            sqlCommand.Parameters.AddWithValue("@date", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(this.Date));
-            sqlCommand.Parameters.AddWithValue("@refNo", RDFramework.Utility.Conversion.SafeSetDatabaseValue<string>(this.ReferenceNumber));
-            sqlCommand.ExecuteNonQuery();
+          while (rd.Read())
+          {
+            validationStatus = RDFramework.Utility.Conversion.SafeReadDatabaseValue<bool>(rd["ValidationStatus"]);
+          }
         }
+      }
     }
 
-    public bool ValidateTrainingRoomScheduleAvailability(int roomID, DateTime startDate, DateTime endDate)
-    {
-        bool validationStatus = false;
+    return validationStatus;
+  }
 
-        using (SqlConnection sqlConnection = new SqlConnection(Settings.iReserveConnectionStringReader))
+  public void CancelTrainingRoomScheduleMapping(SqlConnection sqlConnection, string ccRequestReferenceNo)
+  {
+    using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.CancelTrainingRoomScheduleMapping, sqlConnection))
+    {
+      sqlCommand.CommandType = CommandType.StoredProcedure;
+      sqlCommand.Parameters.AddWithValue("@refNo", RDFramework.Utility.Conversion.SafeSetDatabaseValue<string>(ccRequestReferenceNo));
+      sqlCommand.ExecuteNonQuery();
+    }
+  }
+
+  public bool ValidateSummaryTrainingRoomScheduleAvailability(List<TrainingRoomRequest> trainingRoomRequestList)
+  {
+    bool validationStatus = true;
+
+    foreach (TrainingRoomRequest trainingRoomRequest in trainingRoomRequestList)
+    {
+      using (SqlConnection sqlConnection = new SqlConnection(Settings.iReserveConnectionStringReader))
+      {
+        using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.ValidateTrainingRoomScheduleAvailability, sqlConnection))
         {
-            using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.ValidateTrainingRoomScheduleAvailability, sqlConnection))
+          sqlConnection.Open();
+          sqlCommand.CommandType = CommandType.StoredProcedure;
+          sqlCommand.Parameters.AddWithValue("@roomID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(trainingRoomRequest.PartitionID));
+          sqlCommand.Parameters.AddWithValue("@startDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(trainingRoomRequest.StartDate));
+          sqlCommand.Parameters.AddWithValue("@endDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(trainingRoomRequest.EndDate));
+
+          using (SqlDataReader rd = sqlCommand.ExecuteReader())
+          {
+            while (rd.Read())
             {
-                sqlConnection.Open();
-                sqlCommand.CommandType = CommandType.StoredProcedure;
-                sqlCommand.Parameters.AddWithValue("@roomID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(roomID));
-                sqlCommand.Parameters.AddWithValue("@startDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(startDate));
-                sqlCommand.Parameters.AddWithValue("@endDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(endDate));
-
-                using (SqlDataReader rd = sqlCommand.ExecuteReader())
-                {
-                    while (rd.Read())
-                    {
-                        validationStatus = RDFramework.Utility.Conversion.SafeReadDatabaseValue<bool>(rd["ValidationStatus"]);
-                    }
-                }
+              validationStatus = RDFramework.Utility.Conversion.SafeReadDatabaseValue<bool>(rd["ValidationStatus"]);
             }
+          }
         }
+      }
 
-        return validationStatus;
+      if (validationStatus == false)
+      {
+        return false;
+      }
     }
 
-    public void CancelTrainingRoomScheduleMapping(SqlConnection sqlConnection, string ccRequestReferenceNo)
+    return validationStatus;
+  }
+
+  public void TranTrainingRoomScheduleMapping(int type, SqlConnection sqlConnection)
+  {
+    using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.TranTrainingRoomScheduleMapping, sqlConnection))
     {
-        using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.CancelTrainingRoomScheduleMapping, sqlConnection))
-        {
-            sqlCommand.CommandType = CommandType.StoredProcedure;
-            sqlCommand.Parameters.AddWithValue("@refNo", RDFramework.Utility.Conversion.SafeSetDatabaseValue<string>(ccRequestReferenceNo));
-            sqlCommand.ExecuteNonQuery();
-        }
+      sqlCommand.CommandType = CommandType.StoredProcedure;
+      sqlCommand.Parameters.AddWithValue("@type", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(type));
+      sqlCommand.Parameters.AddWithValue("@partitionID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(this.PartitionID));
+      sqlCommand.Parameters.AddWithValue("@date", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(this.Date));
+      sqlCommand.Parameters.AddWithValue("@refNo", RDFramework.Utility.Conversion.SafeSetDatabaseValue<string>(this.ReferenceNumber));
+      sqlCommand.ExecuteNonQuery();
     }
+  }
 
-    public bool ValidateSummaryTrainingRoomScheduleAvailability(List<TrainingRoomRequest> trainingRoomRequestList)
-    {
-        bool validationStatus = true;
-
-        foreach (TrainingRoomRequest trainingRoomRequest in trainingRoomRequestList)
-        {
-            using (SqlConnection sqlConnection = new SqlConnection(Settings.iReserveConnectionStringReader))
-            {
-                using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.ValidateTrainingRoomScheduleAvailability, sqlConnection))
-                {
-                    sqlConnection.Open();
-                    sqlCommand.CommandType = CommandType.StoredProcedure;
-                    sqlCommand.Parameters.AddWithValue("@roomID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(trainingRoomRequest.PartitionID));
-                    sqlCommand.Parameters.AddWithValue("@startDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(trainingRoomRequest.StartDate));
-                    sqlCommand.Parameters.AddWithValue("@endDate", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(trainingRoomRequest.EndDate));
-
-                    using (SqlDataReader rd = sqlCommand.ExecuteReader())
-                    {
-                        while (rd.Read())
-                        {
-                            validationStatus = RDFramework.Utility.Conversion.SafeReadDatabaseValue<bool>(rd["ValidationStatus"]);
-                        }
-                    }
-                }
-            }
-
-            if (validationStatus == false)
-            {
-                return false;
-            }
-        }
-
-        return validationStatus;
-    }
-
-    public void TranTrainingRoomScheduleMapping(int type, SqlConnection sqlConnection)
-    {
-        using (SqlCommand sqlCommand = new SqlCommand(StoredProcedures.TranTrainingRoomScheduleMapping, sqlConnection))
-        {
-            sqlCommand.CommandType = CommandType.StoredProcedure;
-            sqlCommand.Parameters.AddWithValue("@type", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(type));
-            sqlCommand.Parameters.AddWithValue("@partitionID", RDFramework.Utility.Conversion.SafeSetDatabaseValue<int>(this.PartitionID));
-            sqlCommand.Parameters.AddWithValue("@date", RDFramework.Utility.Conversion.SafeSetDatabaseValue<DateTime>(this.Date));
-            sqlCommand.Parameters.AddWithValue("@refNo", RDFramework.Utility.Conversion.SafeSetDatabaseValue<string>(this.ReferenceNumber));
-            sqlCommand.ExecuteNonQuery();
-        }
-    }
-
-    #endregion
+  #endregion
 }
